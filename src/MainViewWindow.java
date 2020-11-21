5f1a6be08694df1b84a55c51878ef71cfc203d2f
Matin Massoudi <matin@massoudi.net>
2020-11-20 17:22:25 -0700

    MainViewWindow skeleton code

--- src/MainViewWindow.java
+++ src/MainViewWindow.java
@@ -1,10 +1,28 @@
 /**
- * Authors: 
- *
+ * MainViewWindow.java creates canvas and aggregates all the views.
+ * 
  *
  */
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class MainViewWindow {
+public class MainViewWindow extends JFrame{
+	
+	/**
+	 * Default constructor.
+	 */
+	MainViewWindow()
+	{
+		MenuBar menuBar = new MenuBar();
+		add(menuBar);
+		pack();
+		setVisible(true);
+	}
+	
 
 	/**
 	 * Main Method
@@ -12,6 +30,10 @@ public class MainViewWindow {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		MainViewWindow window = new MainViewWindow();
+		window.setSize(400,300);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
 		
 	}
 
