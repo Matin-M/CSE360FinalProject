ece10d56f89ed14d95b8342f6d1477eb2f51edb1
Alex Gaouette <agaouett@asu.edu>
2020-11-25 20:28:40 -0700

    Now generates attendance data

--- generateRoster/generate.py
+++ generateRoster/generate.py
@@ -3,9 +3,10 @@ import random
 asuID = 1210000000
 fNames = []
 lNames = []
-programs = ["Computer Science", "Computer Systems Engineering"]
-ACADEMIC_LEVEL = "Undergrad"
+PROGRAMS = ["Computer Science", "Computer Systems Engineering"]
+ACADEMIC_LEVEL = "Undergrad, Graduate"
 asurite = []
+team = ["agaouette", "mmassoudi", "dragipi", "orios", "cwoehler"]
 
 numStudents = int(input("How many students should I generate? (max 300): "))
 
@@ -26,12 +27,36 @@ if numStudents > 0 and numStudents < 301:
         randomFirst = random.choice(fNames)
         randomLast =  random.choice(lNames)
         asuriteSTR = randomFirst[0].lower() + randomLast.lower()
-        program = random.choice(programs)
+        program = random.choice(PROGRAMS)
+        level = random.choice(ACADEMIC_LEVEL)
         if asuriteSTR not in asurite:
             asurite.append(asuriteSTR)
         else: 
             while asuriteSTR in asurite:
                 asuriteSTR = asuriteSTR + str(random.randint(0, 100))
             asurite.append(asuriteSTR)
-        roster.write(str(addID) + "," + randomFirst + "," + randomLast + "," + program + "," + ACADEMIC_LEVEL+ "," + asurite[i] + "\n")
+        roster.write(str(addID) + "," + randomFirst + "," + randomLast + "," + program + "," + level + "," + asurite[i] + "\n")
     roster.close()
+
+    attendance = open("attendance.csv", "w")
+    for student in asurite:
+        # Check if asurite contains a number if it does we'll make them connect
+        # multiple times, they'll connect between 1-5 times for a maximum of
+        # 75 minutes
+        if any(map(str.isdigit, student)):
+           bound = random.randint(1, 5)
+           for i in range (1, bound):
+               time = random.randint(1, 15)
+               attendance.write(student + "," + str(time) + "\n")
+        else:
+            time = random.randint(1, 75)
+            attendance.write(student + "," + str(time) + "\n")
+
+    flip = random.randint(0,1)
+    if flip == 1:
+        # One of us is in the wrong class
+        visitor = random.choice(team)
+        time = random.randint(1, 75)
+        attendance.write(visitor + "," + str(time) + "\n")
+
+    attendance.close()
