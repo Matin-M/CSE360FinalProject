0fa0f2cd9d354f5d16afdd0a1a160cda6cf7e9cb
Matin Massoudi <matin@massoudi.net>
2020-11-25 21:55:52 -0800

    Added DataPlotPanel.java

---
+++ src/DataPlotPanel.java
@@ -0,0 +1,24 @@
+import java.awt.Color;  
+import javax.swing.JPanel;  
+import javax.swing.SwingUtilities;  
+import javax.swing.WindowConstants;  
+import org.jfree.chart.ChartFactory;  
+import org.jfree.chart.ChartPanel;  
+import org.jfree.chart.JFreeChart;  
+import org.jfree.chart.plot.XYPlot;  
+import org.jfree.data.xy.XYDataset;  
+import org.jfree.data.xy.XYSeries;  
+import org.jfree.data.xy.XYSeriesCollection; 
+
+public class DataPlotPanel extends JPanel{
+	//Serial version ID.
+	private static final long serialVersionUID = 6294689542092367723L;
+	XYDataset dataSet;
+	JFreeChart chart 
+
+	public DataPlotPanel() {
+		// TODO Auto-generated constructor stub
+		
+	}
+
+}
