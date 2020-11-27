368bebb6448f51370ec486d059919708a268eba1
Alex Gaouette <agaouett@asu.edu>
2020-11-27 11:02:50 -0700

    Roster functions have been updated. Parses attendance

--- src/RosterManager.java
+++ src/RosterManager.java
@@ -23,13 +23,14 @@ public class RosterManager extends JFrame {
 		chooser = new JFileChooser();
 		filter = new FileNameExtensionFilter("csv files", "csv");
 		chooser.addChoosableFileFilter(filter);
+		
+	}
+	
+	public void openFile()
+	{
 		chooser.setDialogTitle("Choose a file");
 		this.getContentPane().add(chooser);
 		chooser.setVisible(true);
-	}
-	
-	public ArrayList<ArrayList<String>> openFile()
-	{
 	    int returnVal = chooser.showOpenDialog(this);
 	    
 	    if (returnVal == JFileChooser.APPROVE_OPTION)
@@ -41,13 +42,24 @@ public class RosterManager extends JFrame {
 		    	BufferedReader bReader;
 		    	final String DELIMITER = ",";
 		    	String line;
-		    	ArrayList<String> tempList;
+				ArrayList<String> tempList, columnNames;
 		    	
 		    	csvRoster = chooser.getSelectedFile();
 		    	fReader = new FileReader(csvRoster);
 		    	bReader = new BufferedReader(fReader);
 	    		
-		    	line = "";
+				line = "";
+
+				// Add column names to roster to be used in DataTable
+				columnNames = new ArrayList<String>();
+				columnNames.add("ID");
+				columnNames.add("First Name");
+				columnNames.add("Last Name");
+				columnNames.add("Program");
+				columnNames.add("Level");
+				columnNames.add("ASURITE");
+				roster.add(columnNames);
+
 	    		while ((line = bReader.readLine()) != null)
 	    		{
 					// Create an ArrayList of Strings to hold all data fields
@@ -72,6 +84,80 @@ public class RosterManager extends JFrame {
 	    	}
 		}
 		
+	}
+
+	public void addAttendance()
+	{
+		chooser.setDialogTitle("Choose a file");
+		this.getContentPane().add(chooser);
+		chooser.setVisible(true);
+	    int returnVal = chooser.showOpenDialog(this);
+	    
+	    if (returnVal == JFileChooser.APPROVE_OPTION)
+	    {	    	
+	    	try 
+	    	{
+	    		File attendance;
+		    	FileReader fReader;
+		    	BufferedReader bReader;
+		    	final String DELIMITER = ",";
+		    	String line, date;
+				String[] tempArr;
+				int loc, i, rowSize, columnSize;
+				boolean found;
+		    	
+		    	attendance = chooser.getSelectedFile();
+		    	fReader = new FileReader(attendance);
+				bReader = new BufferedReader(fReader);
+				// Add the new column. We need a date picker for this
+				roster.get(0).add("November 26");
+				columnSize = roster.get(0).size();
+				
+				// All students start at zero minutes of attendance
+				for (i = 1; i < roster.size(); ++i)
+				{
+					roster.get(i).add("0");
+				}
+
+				line = "";
+	    		while ((line = bReader.readLine()) != null)
+	    		{
+					// Create an array of Strings to hold asurite and time connected
+					// from the attendance CSV file
+					tempArr = line.split(DELIMITER);
+
+					
+					// We don't need to check the column names
+					i = 1;
+					found = false;
+
+					while (found != true && i != roster.size())
+					{
+						// asurite matches
+						if (tempArr[0].equals(roster.get(i).get(5)))
+						{
+							found = true;
+							int currentVal = Integer.parseInt(roster.get(i).get(columnSize - 1));
+							currentVal += Integer.parseInt(tempArr[1]);
+							String updated = Integer.toString(currentVal);
+							roster.get(i).set(columnSize - 1, updated);
+						}
+						++i;
+					}  			
+	    		}
+	    		bReader.close();
+				fReader.close();
+				
+	    	}
+	    	catch (IOException ioe)
+	    	{
+				System.out.println(ioe.getLocalizedMessage());
+			}
+		}
+	}
+
+	public ArrayList<ArrayList<String>> getRoster()
+	{
 		return roster;
 	}
 }
